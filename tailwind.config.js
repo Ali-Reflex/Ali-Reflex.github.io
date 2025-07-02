d8eacfdbf005bf290a34130f000d2eff6e1490e6
Ali-Reflex <vampier1999.da@gmail.com>
2025-07-02 18:22:55 +0330

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx,vue}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ffe5ec',
+      },
+    },
+  },
+  plugins: [],
+}
